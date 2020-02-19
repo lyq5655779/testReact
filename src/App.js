68272093d86323665b7d7b8e6bea70646b0f564b
lyq5655779 <liyanqinghi@163.com>
2020-02-19 18:28:55 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="row">
+      <div classNmae="col-xs-l col-xs-offset-11">
+        <h1>Hello11111</h1>
+
+      </div>
+    </div>
+  </div>
+   
+  );
+}
+
+
+
+export default App;
